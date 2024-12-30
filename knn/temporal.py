87c67af645dc021e6da5acdb6339095671d469d1
Ugne Adamo <120172293+uadamo@users.noreply.github.com>
2024-12-30 10:56:42 +0100

    feat: random forrest classifier

---
+++ knn/temporal.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.append('..')
+import torch
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+
+print("hello")
